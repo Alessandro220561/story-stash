9cf855cfcf49c73ba2623c301742b4af1f171fb9
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2024-02-23 17:00:51 -0500

    create new books table/model

--- server/models.py
+++ server/models.py
@@ -10,3 +10,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
     email = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User {self.id}, {self.username}, {self.email}>'
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    author = db.Column(db.String)
+    genre = db.Column(db.String)
+    pages = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Book {self.id}, {self.title}, {self.author}, {self.genre}, {self.pages}>'
