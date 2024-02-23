ffff277fce0ad369d611f00aa6f4e81bd1e27097
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2024-02-23 16:57:22 -0500

    create empty users table/ model

--- server/models.py
+++ server/models.py
@@ -4,3 +4,9 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    email = db.Column(db.String)
